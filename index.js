e229cf05852d0354a8ad9af7b9a753e3decc7f7c
HenryNascimentoSilva <henrynascimentosilva@gmail.com>
2023-10-15 13:10:38 -0300

    criando tabela do vençedor

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function getComputerChoice(){
     return choice;
 }
 
+let playerPoints = 0, computerPoints = 0;
+
 function playRound(playerSelection, computerSelection){
     
     switch (playerSelection){
@@ -14,18 +16,20 @@ function playRound(playerSelection, computerSelection){
             }
             
             else if (computerSelection == 2){
+                computerPoints++;
                 return "Computer wins!";
-
             }
             
             else {
+                playerPoints++;
                 return "You won!";
             }
 
         case 2:
 
             if (computerSelection == 1){
-                return "You won!";
+                playerPoints++;
+                return "You won!";  
             }
 
             else if (computerSelection == 2){
@@ -33,39 +37,59 @@ function playRound(playerSelection, computerSelection){
             }
 
             else {
+                computerPoints++;
                 return "Computer wins!";
+                
             }
 
         case 3:
 
             if (computerSelection == 3){
+                computerPoints++;
                 return "Computer wins!";
+                
             }
 
             else if (computerSelection == 2){
+                playerPoints++;
                 return "You won!";
+                
             }
 
             else {
                 return "Draw!";
-
             }
     }
 }
 
 
-
 function game(){
     for(let i = 0; i < 5; i++){
+
         playerSelection = parseInt(prompt("Rock, Paper, Scissors! \n Choose -1(Rock), 2-(Paper), 3-(Scissors)"));
-        computerSelection = getComputerChoice()
+        computerSelection = getComputerChoice();
+        playRounds = playRound(playerSelection, computerSelection);
+
         if (playerSelection > 0 && playerSelection < 4){
-            console.log(playRound(playerSelection, computerSelection));
+            console.log(playRounds);
         }
+        
         else {
             console.error("number not in 1, 2 or 3");
             break;
         }
+    }
+
+    if (playerPoints > computerPoints) {
+        console.log("You have won the computer!");;
+    }
+
+    else if (computerPoints > playerPoints) {
+        console.log("The computer has won!");
+    }
+
+    else {
+        console.log("Draw!");
     }
 }
 
