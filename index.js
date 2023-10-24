94ff169a622c2fb28125f2a332c9808ec9675d7e
HenryNascimentoSilva <henrynascimentosilva@gmail.com>
2023-10-23 21:27:38 -0300

    formatando melhor o documento

--- index.js
+++ index.js
@@ -1,97 +1,69 @@
-function getComputerChoice(){
-    const choice = Math.floor(Math.random() * 3) + 1;
-    return choice;
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3) + 1;
+  return choice;
 }
 
-let playerPoints = 0, computerPoints = 0;
+let playerPoints = 0,
+  computerPoints = 0;
 
-function playRound(playerSelection, computerSelection){
-    
-    switch (playerSelection){
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection) {
+    case 1:
+      if (computerSelection == 1) {
+        return "Draw, no points!";
+      } else if (computerSelection == 2) {
+        computerPoints++;
+        return "Point for computer!";
+      } else {
+        playerPoints++;
+        return "Point for you!";
+      }
 
-        case 1:
+    case 2:
+      if (computerSelection == 1) {
+        playerPoints++;
+        return "Point for you!";
+      } else if (computerSelection == 2) {
+        return "Draw, no points!";
+      } else {
+        computerPoints++;
+        return "Point for computer!";
+      }
 
-            if (computerSelection == 1){
-                return "Draw, no points!";
-            }
-            
-            else if (computerSelection == 2){
-                computerPoints++;
-                return "Point for computer!";
-            }
-            
-            else {
-                playerPoints++;
-                return "Point for you!";
-            }
-
-        case 2:
-
-            if (computerSelection == 1){
-                playerPoints++;
-                return "Point for you!";  
-            }
-
-            else if (computerSelection == 2){
-                return "Draw, no points!";
-            }
-
-            else {
-                computerPoints++;
-                return "Point for computer!";
-                
-            }
-
-        case 3:
-
-            if (computerSelection == 3){
-                computerPoints++;
-                return "Point for computer!";
-                
-            }
-
-            else if (computerSelection == 2){
-                playerPoints++;
-                return "Point for you!";
-                
-            }
-
-            else {
-                return "Draw, no points!";
-            }
-    }
+    case 3:
+      if (computerSelection == 3) {
+        computerPoints++;
+        return "Point for computer!";
+      } else if (computerSelection == 2) {
+        playerPoints++;
+        return "Point for you!";
+      } else {
+        return "Draw, no points!";
+      }
+  }
 }
-
 
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id == 1) {
+      playerSelection = 1;
+    } else if (button.id == 2) {
+      playerSelection = 2;
+    } else {
+      playerSelection = 3;
+    }
 
-    button.addEventListener('click', () => {
-        if (button.id == 1){
-            playerSelection = 1;
-        }
+    let computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
 
-        else if (button.id == 2){
-            playerSelection = 2;
-        }
-
-        else{
-            playerSelection = 3;
-        }
-
-        let computerSelection = getComputerChoice();
-        const result = (playRound(playerSelection, computerSelection));
-
-        if (result.includes("Point for you!")) {
-            playerScore.textContent = playerPoints;
-        } else if (result.includes("Point for computer!")) {
-            computerScore.textContent = computerPoints;
-        }
-
-        console.log(result);
-      });
-
+    if (result.includes("Point for you!")) {
+      playerScore.textContent = playerPoints;
+    } else if (result.includes("Point for computer!")) {
+      computerScore.textContent = computerPoints;
+    }
+  });
 });
